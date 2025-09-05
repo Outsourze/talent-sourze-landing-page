e284a7930967953656ddc3938feb6320379ec7b7
DelrosarioRovic <rovicdelrosario01@gmail.com>
2025-09-05 12:19:44 +0800

    fix

--- src/component/layout.js
+++ src/component/layout.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "./header";
-import Footer from "@/component/footer/footer-container"
+import Footer from "@/component/footer/FooterContainer"
 import { openSans, raleWay, sourceCode, jakarta } from '../utility/font';
 
 export default function Layout({ children }) {
